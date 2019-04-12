364b493177d38affa372e467902eb81c747ccf7b
winniefong1021 <winniefong21@gmail.com>
2019-04-12 11:26:02 -0700

    Solved addNode, hasEdge, addEdge and removeEdge

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,16 +2,13 @@
 
 // Instantiate a new graph
 var Graph = function(value) {
-  this.value = value;
-  this.fromNodeArr = [];
-  this.toNodeArr = [];
-  this.collection = [];
+  this.collection = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
   var newNode = new Graph(node);
-  this.collection.push(newNode);
+  this.collection[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
@@ -21,35 +18,53 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-// 5th
+  // create a holder var for deleted key
+  // iterate over collection
+    // if key = node
+      // iterate through removeNode's array (this.collection[key])
+      // invoke this.prototype.removeEdge(node, i)
+      // invoke this.prototype.removeEdge(i, node)
+  // delete collection key === node
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-// 3rd
+  for (let k in this.collection) {
+    if (k == fromNode) {
+      if (this.collection[k].includes(toNode)) {
+        return true;
+      }       
+    }
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  // iterate through this.collection arr to grab each node
-  var fromFound = this.collection.indexOf(fromNode); // need to fix lines 35 to 41
-  if (fromFound >= 0) {
-    this.collection[fromFound].toNodeArr.push(toNode);
-  }
-  var toFound = this.collection.indexOf(toNode);
-  if (toFound >= 0) {
-    this.collection[toFound].fromNodeArr(toNode);
+  for (var k in this.collection) {
+    if (k == fromNode) {
+      this.collection[fromNode].push(toNode);
+      this.collection[toNode].push(fromNode);
+    }
   }
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-// 4th
-// find indexOf toNode value in fromNode's end arr
-  // splice out toNode from end arr at that index
-// find indexOf fromNode value in toNode's start arr
-  // splice out fromNode from start arr at that index
-  
+  for (var k in this.collection) {
+    if (k == fromNode) {
+      if (this.collection[k].includes(toNode)) {
+        var toNodeIdx = this.collection[k].indexOf(toNode);
+        this.collection[k].splice(toNodeIdx);
+      }
+    }
+    if (k == toNode) {
+      if (this.collection[k].includes(fromNode)) {
+        var fromNodeIdx = this.collection[k].indexOf(fromNode);
+        this.collection[k].splice(fromNodeIdx);
+      }
+    }
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
