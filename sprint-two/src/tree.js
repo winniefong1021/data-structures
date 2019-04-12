fefdcbe7ea879ebef1954d89015ba8c134ef25f0
winniefong1021 <winniefong21@gmail.com>
2019-04-11 18:23:58 -0700

    Solved tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -15,42 +15,28 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
-  // create a variable called containIt which is set to false;
-  // check if parent node.value === target,
-    // return true;
-  
-  //helper function that starts at the parent node, iterates over it's children
-    // if children.node.length == 0, return containIt;
-    // if there is children, iterate over each element/child
-      //  check if current node's value == target
-        //  if so, containIt = true;
-        // return containIt;
-      // else if value == target, 
-        // recursively look at that current node's children array
-    //  
-    // return containIt;
   var containIt = false;
   if (this.value === target) {
     return true;
   }
- // debugger;
+  if (this.children.length === 0) {
+    return containIt;
+  }
 
-  var findTarget = function(value) {
-    if (this.children.length === 0) {
-      return containIt;
-    } else {
-      for (var i = 0; i < this.children.length; i++) {
-        var child = this.children[i];
-        if (child.value === target) {
-          containIt = true;
-          return containIt;  
-        } else {
-          findTarget(child);
-        } 
-      }
+  var findTarget = function(node) {
+    for (var i = 0; i < node.length; i++) {
+      var child = node[i];
+      if (child.value === target) {
+        containIt = true;
+        return containIt;  
+      } else {
+        findTarget(child.children);
+      } 
     }
   }
-  findTarget(this);
+  
+
+  findTarget(this.children);
   return containIt;
 };
 
