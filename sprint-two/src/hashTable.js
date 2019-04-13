af02a21c17bd9efd7f1f967252f6ea9edb4e26e6
winniefong1021 <winniefong21@gmail.com>
2019-04-12 19:58:36 -0700

    Solved HashTable data structure

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,47 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tempBucket = [];
+  if (Array.isArray(this._storage.get(index))) {
+    var convertValue = this._storage.get(index);
+    convertValue.forEach(tupplePair => tempBucket.push(tupplePair));
+    var takeFromHere = tempBucket.indexOf(k);
+    if (takeFromHere >= 0) {
+      tempBucket.splice(takeFromHere+1, 1, v);
+    } else {
+      tempBucket.push(k,v);
+    }
+    this._storage.set(index, tempBucket);      
+  } else {
+    this._storage.set(index,[k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var extractedBucket = this._storage.get(index);
+  for (let i = 0; i < extractedBucket.length; i++) {
+    if (extractedBucket[i] === k) {
+      return extractedBucket[i+1];   
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var extractedBucket = this._storage.get(index);
+  for (let i = 0; i < extractedBucket.length; i++) {
+    if (extractedBucket[i] === k) {
+      var takeFromHere = extractedBucket.indexOf(k);
+      extractedBucket.splice(takeFromHere,2);   
+    }
+  }
+  this._storage.set(index, extractedBucket);  
 };
 
 
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the 
+above functions?
  */
-
-
