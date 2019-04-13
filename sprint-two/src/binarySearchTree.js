2a2cd576f3c6ab3a62dcffd1bea535e6c44807e2
winniefong1021 <winniefong21@gmail.com>
2019-04-13 12:27:09 -0700

    Solved binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,45 +1,83 @@
 var BinarySearchTree = function(value) {
-  this.value = value;
-  this.left = null;
-  this.right = null;
-  this.contains = function(){};
-  this.depthFirstLog = function(){};
-  this.insert = function(value) {
-    var newNode = new binaryNode(value);
-    var placer = function(nodeInQuestion) {
-      var currentNode = nodeInQuestion; 
-      if (currentNode.value > value && (currentNode.left === null && currentNode.right === null)) {
-        currentNode.left = newNode;
-      }
-      if (currentNode.value < value && (currentNode.left === null && currentNode.right === null)) {
-        currentNode.right = newNode;
-      }
-      if (currentNode.left.value > value) {
-        placer(currentNode.left);
-      }
-      if (currentNode.left.value < value && currentNode.right === null) {
-        currentNode.right = newNode;
-      }
-      if (currentNode.right.value < value) {
-        placer(currentNode.right);
-      }
-      if (currentNode.right.value > value && currentNode.left === null) {
-        currentNode.left = newNode;
-      }
-    }  
-    placer(this);
-  };
+  var tree = Object.create(binaryTreeMethods);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  return tree;
 };
-  
 
-var binaryNode = function(value) {
-  this.value = value;
-  this.left = null; 
-  this.right = null;
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value) {
+  var newNode = BinarySearchTree(value);
+  var inserter = function(node) {
+    if (newNode.value === node.value) return;
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else if (newNode.value < node.left.value) {
+        inserter(node.left.left);
+      } else if (newNode.value > node.left.value) {
+        if (node.left.right === null) {
+          node.left.right = newNode;
+        } else {
+          inserter(node.left.right);
+        }
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else if (newNode.value < node.right.value) {
+        if (node.right.left === null) {
+          node.right.left = newNode;
+        } else {
+          inserter(node.right.left);
+        }
+      } else if (newNode.value > node.right.value) {
+        inserter(node.right.left);
+      }     
+    }
+  };
+  inserter(this);
+};
+
+binaryTreeMethods.contains = function(value) {
+  var containIt = false;   
+  var finder = function(node) {
+    if (node.value === value) {
+      containIt = true;
+      return containIt;
+    }
+    if (node.left === null || node.right === null) {
+      return containIt;
+    }
+    if (value > node.value) {
+      finder(node.right);
+    } else {
+      finder(node.left);
+    }
+  };
+  
+  finder(this);
+  return containIt;
+};
+
+binaryTreeMethods.depthFirstLog = function(cb) {
+  var eachNode = function(node) {
+    cb(node.value);
+    if (node.left !== null) {
+      eachNode(node.left);
+    }
+    if (node.right !== null) {
+      eachNode(node.right);
+    }
+  };
+  eachNode(this);
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+All functions are O(n)
  */
