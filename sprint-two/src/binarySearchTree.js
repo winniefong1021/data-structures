13451709301d361bd8df9bcdd28d160549383dae
winniefong1021 <winniefong21@gmail.com>
2019-04-12 21:47:01 -0700

    Solved HashTable data structure

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,43 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+  this.contains = function(){};
+  this.depthFirstLog = function(){};
+  this.insert = function(value) {
+    var newNode = new binaryNode(value);
+    var placer = function(nodeInQuestion) {
+      var currentNode = nodeInQuestion; 
+      if (currentNode.value > value && (currentNode.left === null && currentNode.right === null)) {
+        currentNode.left = newNode;
+      }
+      if (currentNode.value < value && (currentNode.left === null && currentNode.right === null)) {
+        currentNode.right = newNode;
+      }
+      if (currentNode.left.value > value) {
+        placer(currentNode.left);
+      }
+      if (currentNode.left.value < value && currentNode.right === null) {
+        currentNode.right = newNode;
+      }
+      if (currentNode.right.value < value) {
+        placer(currentNode.right);
+      }
+      if (currentNode.right.value > value && currentNode.left === null) {
+        currentNode.left = newNode;
+      }
+    }  
+    placer(this);
+  };
 };
+  
+
+var binaryNode = function(value) {
+  this.value = value;
+  this.left = null; 
+  this.right = null;
+};
+
 
 
 /*
