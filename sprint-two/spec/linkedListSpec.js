c5ab76264952187c81707e164f215e94d119eaf6
winniefong1021 <winniefong21@gmail.com>
2019-04-13 14:57:46 -0700

    Add test checking for contains on more than 2 nodes

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -51,5 +51,11 @@ describe('linkedList', function() {
     expect(linkedList.contains(4)).to.equal(false);
   });
 
-  // add more tests here to test the functionality of linkedList
+  it('should return a node\'s value where there are more than two nodes in the list', function() {
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    linkedList.addToTail(6);
+    linkedList.addToTail(7);
+    expect(linkedList.contains(6)).to.equal(true);
+  });
 });
